32593af3e40a0c7297a933c4255991b726f8d326
Zac <a19860101@gmail.com>
2021-11-26 10:57:53 +0800

    20211126

--- _javascript/variable.js
+++ _javascript/variable.js
@@ -2,9 +2,14 @@
 
 var x = 100;
 
+var x = 'hello';
+
 let y = 123;
 
+y = 321;
+
 console.log(y);
+
 
 /* 
     變數命名注意事項:
@@ -13,4 +18,19 @@ console.log(y);
     3. 不可使用特殊符號
     4. 不可使用保留字
 */
+
+
+// 常數
+// 不可重新賦值
+
+const z = 666;
+
+/* 
+    var vs let,const
+    1. var可以重複宣告，let,const則否
+    2. var是function scope，let,const則是block scope
+
+*/
+
+
  
