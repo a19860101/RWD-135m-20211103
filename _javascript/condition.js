65bba7d5745b19a9b41e1a9600b1b8c26be43b47
Zac <a19860101@gmail.com>
2021-11-26 12:20:26 +0800

    20211126

---
+++ _javascript/condition.js
@@ -0,0 +1,35 @@
+// 判斷式
+let x = 0;
+
+// if()
+
+if(x > 0){
+    console.log('success');
+}
+
+// if...else
+
+if(x > 0){
+    console.log('success2');
+}else{
+    console.log('error');
+}
+
+
+// if...else if
+
+if(x > 0){
+    console.log('大於零')
+}else if(x < 0){
+    console.log('小於零')
+}
+
+// if...else if...else
+if(x > 0){
+    console.log('大於零')
+}else if(x < 0){
+    console.log('小於零')
+}else{
+    console.log('error2')
+}
+
