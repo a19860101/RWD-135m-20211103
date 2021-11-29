29072d4429c10d6e909a4eefc31efc99042a2830
Zac <a19860101@gmail.com>
2021-11-29 12:19:29 +0800

    20211129

---
+++ _javascript/function.js
@@ -0,0 +1,19 @@
+function test(){
+    alert('hello test');
+}
+
+function total(){
+    let x = 100;
+    let y = 60;
+    return x + y;
+}
+
+console.log(total());
+
+let square = function(){
+    let w = 100;
+    let h = 50;
+    return w * h;
+}
+
+console.log(square());
