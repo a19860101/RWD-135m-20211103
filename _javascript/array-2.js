1bc13da9602bbbe82b740676458fdc4162b866b4
Zac <a19860101@gmail.com>
2021-11-29 11:46:40 +0800

    20211129

--- _javascript/array-2.js
+++ _javascript/array-2.js
@@ -1,4 +1,4 @@
-let ary = ['紅茶','綠茶','拿鐵','梅子綠茶'];
+let ary = ['紅茶','綠茶','綠茶','拿鐵','梅子綠茶'];
 
 //forEach
 
@@ -13,4 +13,20 @@ let ary_map = ary.map(function(data){
     return data == '梅子綠茶' ? '已售完': data;
 })
 
-console.log(ary_map);
+
+//filter
+let ary_filter = ary.filter(function(data){
+    return data != '綠茶';
+})
+
+//find
+let ary_find = ary.find(function(data){
+    return data == '綠茶';
+})
+
+//findIndex
+let ary_findIndex = ary.findIndex(function(data){
+    return data == '梅子綠茶';
+})
+
+console.log(ary_filter);
