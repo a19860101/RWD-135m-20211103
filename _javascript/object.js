9c8c8da2e5b45a83d9ed69fdf96166acb1b73aca
Zac <a19860101@gmail.com>
2021-12-01 10:11:18 +0800

    20211201

--- _javascript/object.js
+++ _javascript/object.js
@@ -5,11 +5,33 @@
 //     return 100;
 // }
 
-let obj = {
-    name: 'John',
-    mail: 'asdf@gmail.com',
-    active: function(){
-        return 100;
+// let obj = {
+//     name: 'John',
+//     mail: 'asdf@gmail.com',
+//     active: function(){
+//         return 100;
+//     }
+// }
+let drinks = [
+    {
+        name:'紅茶',
+        price: 30
+    },
+    {
+        name:'綠茶',
+        price: 30
+    },
+    {
+        name:'奶茶',
+        price:35
+    },
+    {
+        name:'珍珠奶茶',
+        price: 50
     }
+];
+// console.log(drinks);
+for(let drink of drinks){
+    console.log(drink.name);
+    console.log(drink.price);
 }
-console.log(obj);
