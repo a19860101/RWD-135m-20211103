8b378cf93c46a75af8cdf9d2f6e552e35d6b4165
Zac <a19860101@gmail.com>
2021-12-01 10:00:53 +0800

    20211201

---
+++ _javascript/object.js
@@ -0,0 +1,15 @@
+// let obj = new Object();
+// obj.name = 'John';
+// obj.mail = 'asdf@gmail.com';
+// obj.active = function(){
+//     return 100;
+// }
+
+let obj = {
+    name: 'John',
+    mail: 'asdf@gmail.com',
+    active: function(){
+        return 100;
+    }
+}
+console.log(obj);
