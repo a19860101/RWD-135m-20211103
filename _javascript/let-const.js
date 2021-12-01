8faa95b535ca5d0e6003a2882009a064970a6ece
Zac <a19860101@gmail.com>
2021-12-01 10:35:41 +0800

    20211201

---
+++ _javascript/let-const.js
@@ -0,0 +1,23 @@
+let a = 100;
+a =999;
+
+console.log(a);
+
+// const b = 999;
+// b = 100;
+
+const b = ['apple','banana'];
+b.push('cat');
+// b = [];
+console.log(b);
+
+const c = {
+    name:'john'
+}
+c.mail = 'asdf@gmail.com'
+
+console.log(c);
+
+const d = ()=>{
+    return 100;
+}
