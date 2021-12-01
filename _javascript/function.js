3c5cad70fb5a39d1dce8154ef138239c02382fbe
Zac <a19860101@gmail.com>
2021-12-01 09:34:33 +0800

    20211201

--- _javascript/function.js
+++ _javascript/function.js
@@ -1,22 +1,38 @@
 
-console.log(total());
+// console.log(total());
 
 //函式陳述式 function statement (提升性)
-function test(){
-    alert('hello test');
-}
+// function test(){
+//     alert('hello test');
+// }
 
-function total(){
-    let x = 100;
-    let y = 60;
-    return x + y;
-}
+// function total(){
+//     let x = 100;
+//     let y = 60;
+//     return x + y;
+// }
 
 // 函式表達式 function expression
-let square = function(){
-    let w = 100;
-    let h = 50;
-    return w * h;
+// let square = function(){
+//     let w = 100;
+//     let h = 50;
+//     return w * h;
+// }
+
+// console.log(square());
+
+
+function square(x){
+    return x * x;
 }
 
-console.log(square());
+// console.log(square(37));
+
+// function total(price,tax){
+//     return price * tax;
+// }
+
+let total = function(price,tax){
+    return price * tax;
+}
+console.log(total(999,1.1));
