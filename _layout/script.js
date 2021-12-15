81c13267a235ec92d03241c4f5a5c000f1b475ba
Zac <a19860101@gmail.com>
2021-12-15 10:07:08 +0800

    20211215

--- _layout/script.js
+++ _layout/script.js
@@ -11,4 +11,22 @@ $(function(){
             $('.nav-menu').hide();
         }
     });
+    $('.nav-menu').find('a').click(function(){
+        let href=$(this).attr('href');
+        if(href === '#'){
+            $('html,body').animate({
+                scrollTop: 0
+            })
+            return 
+        }
+        let distance = $(href).offset().top;
+        $('html,body').animate({
+            scrollTop: distance
+        })
+    })
+    $('.gotop').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        })
+    })
 })
