788ca593f5bbb9fbd0fb1855078847cea6876b63
Zac <a19860101@gmail.com>
2021-12-15 09:55:41 +0800

    20211215

--- _layout/script.js
+++ _layout/script.js
@@ -3,4 +3,12 @@ $(function(){
         // $('.nav-menu').slideToggle();
         $('.nav-menu').slideToggle();
     })
+    $(window).resize(function(){
+        let w = $(window).width();
+        if(w > 1200){
+            $('.nav-menu').show();
+        }else{
+            $('.nav-menu').hide();
+        }
+    });
 })
